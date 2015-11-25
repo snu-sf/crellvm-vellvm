5bf48fe5924d49f2936b5d11111d31bda62d0d20
Yoonseung Kim <yoonseung.kim@sf.snu.ac.kr>
2015-11-25 16:41:47 +0900

    Add TargetData.t = int in llvmcaml.ml.

--- src/Extraction/llvmcaml.ml
+++ src/Extraction/llvmcaml.ml
@@ -1,4 +1,5 @@
 module TargetData = struct
+  type t = int
   let mgetoffset_aux x y z w = failwith "mgetoffset_aux undef"
   let mgetoffset x y z = failwith "mgetoffset undef"
   let intConsts2Nats x y = failwith "intConsts2Nats undef"
